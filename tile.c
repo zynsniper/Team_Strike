60b2f322eb37cfbc226d719b47051360aee10716
Jacob <sinethembamdluli7274@gmail.com>
2025-03-02 16:26:58 -0400

    changed capitalisation in Tile to tile

--- tile.c
+++ tile.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Tile {
+typedef struct tile {
     char type; // 'P' for palace, 'O' for obstacle, '.' for open space
     int x, y;
-} Tile;
+} tile;
