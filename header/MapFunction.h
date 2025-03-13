17e4f0c58020df3d86f88974f994d13ae2cdd8ac
zynsniper <zynsniper@gmail.com>
2025-03-13 00:19:19 -0300

    included random seed in generateMap function

--- header/MapFunction.h
+++ header/MapFunction.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include "tile.h"
 #include <stdlib.h>
 
@@ -13,6 +14,8 @@ void printMap(Tile gameMap[10][10]) {
 }
 
 void generateMap(Tile gameMap[10][10]){
+    srand(time(NULL));
+
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
             gameMap[i][j].type = '.'; 
