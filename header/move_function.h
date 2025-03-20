5c7ba1c0a4f84c66cad9b0a675e7ce428f872185
zynsniper <zynsniper@gmail.com>
2025-03-20 18:57:15 -0300

    integrated attack option into movement

--- header/move_function.h
+++ header/move_function.h
@@ -4,9 +4,10 @@
 #include "tile.h"
 #include "team.h"
 
-void moveRight(Team* team, Tile gameMap [10][10], int character);
-void moveLeft(Team* team, Tile gameMap [10][10], int character);
-void moveUp(Team* team, Tile gameMap [10][10], int character);
-void moveDown(Team* team, Tile gameMap [10][10], int character);
+void moveRight(Team* team, Team* enemyTeam, Tile gameMap [10][10], int character);
+void moveLeft(Team* team, Team* enemyTeam, Tile gameMap [10][10], int character);
+void moveUp(Team* team, Team* enemyTeam, Tile gameMap [10][10], int character);
+void moveDown(Team* team, Team* enemyTeam, Tile gameMap [10][10], int character);
+void attack(Character * attacker, Character * defender);
 
 #endif
